249fb4a87ade78e4ad01cf2b7f6c9ed52baabc21
HusainMahtab <mahtabh667@gmail.com>
2025-03-03 17:00:54 +0545

    base url change

--- back_end/server.js
+++ back_end/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://lpu-assignment-qsnp.vercel.app'],
+    origin: ['http://localhost:5173','https://lpu-assignment-66d4.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
